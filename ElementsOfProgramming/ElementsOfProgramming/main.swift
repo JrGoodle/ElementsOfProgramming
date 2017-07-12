e6ac928a36dffedaf38e27a9f71b7272c454184f
Joe DeCapo <joe@polka.cat>
2017-07-12 17:29:04 -0500

    Add Xcode project

---
+++ ElementsOfProgramming/ElementsOfProgramming/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ElementsOfProgramming
+//
+//  Created by goodle on 7/12/17.
+//  Copyright © 2017 PolkaDotCat. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
