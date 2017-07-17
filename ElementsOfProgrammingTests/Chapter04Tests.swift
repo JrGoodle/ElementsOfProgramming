3c9771a875585ed5d890d4e6910372aef33e5e46
Joe DeCapo <joe@polka.cat>
2017-07-16 20:10:16 -0500

    Add Chapter 4 tests

---
+++ ElementsOfProgrammingTests/Chapter04Tests.swift
@@ -0,0 +1,23 @@
+//
+//  Chapter04Tests.swift
+//  ElementsOfProgrammingTests
+//
+
+import XCTest
+
+class Chapter04Testts: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func test() {
+        
+    }
+}
